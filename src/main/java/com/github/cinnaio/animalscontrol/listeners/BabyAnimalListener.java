923bd852256ead0f9fec1452bd902941715e6775
Cinnaio <ersynn@outlook.com>
2025-01-22 10:28:07 +0800

    format: 删除了不必要的代码

--- src/main/java/com/github/cinnaio/animalscontrol/listeners/BabyAnimalListener.java
+++ src/main/java/com/github/cinnaio/animalscontrol/listeners/BabyAnimalListener.java
@@ -16,16 +16,11 @@ public class BabyAnimalListener implements Listener {
 
     @EventHandler
     public void onBabyAnimalBorn(EntityBreedEvent event) {
-        // 获取繁殖者（父母）动物
-        Animals parent = (Animals) event.getBreeder();
-
         // 获取新生幼崽
         Animals baby = (Animals) event.getEntity();
 
         // 设置幼崽的喂养时间
         long currentTime = System.currentTimeMillis() / 1000;
         baby.getPersistentDataContainer().set(plugin.getLastFeedKey(), PersistentDataType.LONG, currentTime);
-        
-        // 不发送任何消息，只需确保幼崽的存活时间被正确设置
     }
 }
